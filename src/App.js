4a0e3b367e4bd8834b26dc9a10c119cdb8b81c67
Bakkym <jcamilito6bh2002@gmail.com>
2022-09-24 11:16:50 -0500

    Add index & network routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './components/Inicio'
+import Network from './components/Networks'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Inicio></Inicio>}></Route>
+          <Route path='/network/:id' element={<Network></Network>}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Networks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const networks = () => {
+  return (
+    <div>networks</div>
+  )
+}
+
+export default networks
