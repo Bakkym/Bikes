bfb1e801e030735ed82c1bafcac4844e5b64a297
Bakkym <jcamilito6bh2002@gmail.com>
2022-09-25 00:13:16 -0500

    add chakra network site

--- src/components/Network.jsx
+++ src/components/Network.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { uniqueNetwork } from "../functions/functions";
-
+import { Heading, Box, Text, Image, Flex, Spacer, Tag, Center, Square } from "@chakra-ui/react";
+import logo from "../assets/R.png";
 let countB = 0;
 let countE = 0;
 const Network = () => {
@@ -12,37 +13,65 @@ const Network = () => {
     uniqueNetwork(params.id, setNetwork);
   }, []);
 
-
-    if (network != null) {
-      network.stations.map((station) => {
-        countB += station.free_bikes;
-        countE += station.empty_slots
-      });
-    }
+  if (network != null) {
+    network.stations.map((station) => {
+      countB += station.free_bikes;
+      countE += station.empty_slots;
+    });
+  }
 
   return (
     <>
-      <h2>Bicicletas totales: {countB}</h2>
-      <h2>Espacios totales: {countE}</h2>
+      <center>
+        <Heading as="h1" size="lg">
+          STATIONS
+        </Heading>
+        <Image src={logo} width={300} m={4}></Image>
+      </center>
+      <Box bg="green.200">
+        <Text fontSize="4xl" fontFamily="monospace" p={4}>
+          <p>Bicicletas totales: {countB}</p>
+          <p>Espacios totales: {countE}</p>
+        </Text>
+      </Box>
       {network != null
         ? network.stations.map((station) => (
-            <div key={station.name}>
-
-              <h2>{station.name}</h2>
+            <Box key={station.name} bg="gray.100" p={4} m={4} borderRadius="lg">
+              <strong>{station.name}</strong>
               <p>Fecha actualización: {station.timestamp}</p>
               {station.free_bikes || station.empty_slots != null ? (
-                <div>
-                  <p>Bicicletas libres: {station.free_bikes}</p>
-                  <p>Espacios libres: {station.empty_slots}</p>
-                  <p>
-                    Total de espacios:{" "}
-                    {station.free_bikes + station.empty_slots}
-                  </p>
-                </div>
+                <Box>
+                  <Flex>
+                    <Center w="150px" bg="blue.200">
+                      <Text>Bicicletas libres: {station.free_bikes}</Text>
+                    </Center>
+                    <Center w="150px" bg="yellow.200">
+                      <Text>Espacios libres: {station.empty_slots}</Text>
+                    </Center>
+                    <Center w="170px" bg="green.200">
+                      <Text> Total de espacios:{" "}
+                      {station.free_bikes + station.empty_slots}</Text>
+                    </Center>
+                    
+                    <Spacer />
+                    <Tag p={2} colorScheme="green">
+                      Available
+                    </Tag>
+                  </Flex>
+                </Box>
               ) : (
-                "No está disponible actualmente"
+                <Flex>
+                   <Center w="300px" bg="red.200">
+                      <Text> No está disponible actualmente</Text>
+                    </Center>
+                  
+                  <Spacer />
+                  <Tag p={2} colorScheme="red">
+                    Unavailable
+                  </Tag>
+                </Flex>
               )}
-            </div>
+            </Box>
           ))
         : "No hay estaciones"}
     </>
@@ -50,4 +79,3 @@ const Network = () => {
 };
 
 export default Network;
-
